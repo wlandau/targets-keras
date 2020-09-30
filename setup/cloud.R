7ff33f56ba28fe93c75198f25b14078b1f11ff53
wlandau <will.landau@gmail.com>
2020-09-30 16:12:36 -0400

    Edit setup script

--- setup/cloud.R
+++ setup/cloud.R
@@ -1,33 +1,25 @@
 install.packages(c(
+  "corrr",
   "fst",
   "keras",
+  "qs",
   "recipes",
   "remotes",
   "rmarkdown",
   "rprojroot",
   "rsample",
   "tidyverse",
+  "usethis",
   "visNetwork",
   "yardstick"
 ))
 remotes::install_github("wlandau/targets")
 remotes::install_github("wlandau/tarchetypes")
 root <- rprojroot::find_rstudio_root_file()
-reticulate::install_miniconda(file.path(root, "miniconda"))
-lines <- c(
-  "TAR_SCRIPT_ASK=false",
-  paste0("CONDA=", file.path(root, "miniconda", "bin", "conda")),
-  paste0("WORKON_HOME=", file.path(root, "virtualenvs")),
-  paste0("RETICULATE_PYTHON=", file.path(root, "miniconda", "bin", "python"))
-)
-writeLines(lines, file.path(root, ".Renviron"))
+reticulate::virtualenv_create("r-reticulate")
+line <- "reticulate::use_virtualenv(\"r-reticulate\")"
+write(line, file.path(root, ".Rprofile"), append = TRUE)
 rstudioapi::restartSession()
-reticulate::virtualenv_create(
-  "r-reticulate",
-  python = file.path(root, Sys.getenv("RETICULATE_PYTHON"))
-)
-keras::install_keras(
-  method = "virtualenv",
-  conda = Sys.getenv("CONDA"),
-  envname = "r-reticulate"
-)
+keras::install_keras(method = "virtualenv", envname = "r-reticulate")
+rstudioapi::restartSession()
+tensorflow::tf_config()
